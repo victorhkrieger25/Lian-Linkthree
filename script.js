b47dc6178c1597339010ba3b7acf0b78c86c6b80
victorhkrieger25 <victorhkrieger25@gmail.com>
2026-02-06 22:25:47 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const reveals = document.querySelectorAll('.reveal');
 
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < windowHeight - 80) {
+      el.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
