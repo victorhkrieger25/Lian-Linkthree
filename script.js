21fe1b686df77037c5900c6b8a722223c49b9796
victorhkrieger25 <victorhkrieger25@gmail.com>
2026-02-06 22:31:39 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  /* REVEAL ON SCROLL */
   const reveals = document.querySelectorAll('.reveal');
 
   const revealOnScroll = () => {
@@ -12,4 +14,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   window.addEventListener('scroll', revealOnScroll);
   revealOnScroll();
+
+  /* CARD GLOW FOLLOW */
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    card.addEventListener('mousemove', e => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      card.style.setProperty('--x', `${x}px`);
+      card.style.setProperty('--y', `${y}px`);
+    });
+  });
+
 });
